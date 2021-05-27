a7282ab784dc0d9cdedadaa80f00634f93393c22
Pedro Sebastiaan <pizarropedrov@gmail.com>
2021-05-27 19:39:06 -0400

    add: complement

--- filtro_procesos/filtro_procesos.rb
+++ filtro_procesos/filtro_procesos.rb
@@ -1,23 +1,14 @@
 n = ARGV[0].to_i
 
-data = open("data.csv").readlines
+data = open("data.csv", "r").readlines
+output = open("procesos_filtrados.data", "w")
 
 data.reject! {|x| x.empty?}
 
 a = (data.map do |x| x.to_i end)
 
-b = []
-
-x = a.count
-
-x.times do |i|
-    if a[i] > n
-        b.push a[i]
-    end
+a.each do |i|
+    output.puts(i.to_i) if i.to_i > n
 end
 
-
-open("procesos_filtrados.data", "w") do |csv|
-    csv << b
-end
 
